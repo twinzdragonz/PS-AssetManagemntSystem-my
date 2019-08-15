afbe5ed5a2d001322c19db5077216d7770a70b3a
AZFAR AZMAN <azfarazman95@gmail.com>
2019-08-15 09:41:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PS-AssetManagementSystem-my
+ 
---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import $ from 'jquery';
+import Popper from 'popper.js';
+
+import ThemeSwitcher from './components/themeswitcher.js';
+
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<ThemeSwitcher />, document.getElementById('root'));
+
+
+// Default
+//ReactDOM.render(<App />, document.getElementById('root2'));
+
+
+
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
