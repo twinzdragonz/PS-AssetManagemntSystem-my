102360707f2193645835824a757ab09668f22643
AZFAR AZMAN <azfarazman95@gmail.com>
2019-10-22 18:11:32 +0800

    adding table

--- src/components/Dashboard/module/Profile.jsx
+++ src/components/Dashboard/module/Profile.jsx
@@ -1,13 +1,44 @@
 import React from "react";
+import {Alert,Table} from 'react-bootstrap';
+
+
+
 var Component = React.Component;
-
-
 export default class Profile extends Component{
 
 	render() {
 		return(
-		<div>
-			Hello this is Profile Page
+		<div className = "container">
+			<br></br>
+		 <Table responsive>
+		<thead>
+		<tr>
+			<th></th>
+			<th></th>
+			<th></th>
+			<th></th>
+		</tr>
+		</thead>
+		<tbody>
+		<tr>
+			<td>Username</td>
+			<td>Mark</td>
+			<td>Otto</td>
+			<td>@mdo</td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>@</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td colSpan="2">Larry the Bird</td>
+			<td>@twitter</td>
+		</tr>
+		</tbody>
+		</Table>
 		</div>
 		)
 	}
